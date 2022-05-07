5ca73371e15379496cc052943d5dbbb1a369fc06
Gabriel Correia <101719012+Gabriel06112001@users.noreply.github.com>
2022-05-07 09:30:13 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Header } from './components/Header';
+import { Layout } from './components/Layout';
+
+import './styles/Global.css'
+
+function App() {
+
+	return (
+		<>
+			<Header />
+			<Layout />
+		</>
+		
+	);
+
+}
+
+export default App;
---
+++ src/components/Story/index.js
@@ -0,0 +1,109 @@
+import './style.css'
+
+export function Story() {
+
+    return (
+
+        <div className="container" >
+
+            <div className="user-elements" >
+
+                <div>
+                    <img className="image-user-story" src="https://avatars.githubusercontent.com/u/101719012?v=4"  />
+                </div>
+
+                <span>gabigol</span>
+            </div>
+
+            <div className="user-elements" >
+
+                <div>
+                <img className="image-user-story" src="https://veja.abril.com.br/wp-content/uploads/2022/04/LULA-PODPAH-2021-092.jpeg.jpg?quality=70&amp;strip=info&amp;resize=680,453"  />
+            
+        
+                </div>
+
+                <span>lulaoficial</span>
+
+                
+
+
+            </div>
+            <div className="user-elements" >
+
+                <div>
+                <img className="image-user-story" src="https://www.lance.com.br/galerias/wp-content/uploads/2022/03/luva-de-pedreiro-memes-1-636x474.jpg"  />
+            
+            
+        
+                </div>
+
+                <span>luvadepe</span>
+
+                
+
+            </div>
+            <div className="user-elements" >
+
+                <div>
+                <img className="image-user-story"  src="https://i.pinimg.com/236x/ac/34/92/ac3492c209c3b07ad90e7e0e5dee50ad.jpg"  />
+               
+            
+        
+                </div>
+
+                <span>matuê</span>
+                
+
+
+            </div>
+            <div className="user-elements" >
+
+                <div>
+                <img className="image-user-story"  src="https://omunicipio.com.br/wp-content/uploads/2018/11/ronaldinho-gaucho.jpg" />
+            
+        
+                </div>
+
+                <span>r10oficial</span>
+
+                
+
+
+            </div>
+            <div className="user-elements" >
+
+                <div>
+                <img className="image-user-story"  src="https://www.blogsoestado.com/danielmatos/files/2012/09/zeca-pagodinha.jpg"/>
+            
+        
+                </div>
+
+                <span>zecapago</span>
+
+                
+
+        
+            </div>
+            <div className="user-elements"   src="https://omunicipio.com.br/wp-content/uploads/2018/11/ronaldinho-gaucho.jpg"> 
+
+                <div>
+                <img className="image-user-story"   src="https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blteee1bb651ccd1212/6224cbc4d5564d6eced4accd/Neymar_Paris_Saint-Germain_2021-22.jpg?auto=webp&amp;format=pjpg&amp;quality=80&amp;width=1024" />
+            
+        
+                </div>
+
+                <span>neymarjr</span>
+
+                
+
+
+            </div>
+
+            
+
+        </div>
+
+    )
+
+}
